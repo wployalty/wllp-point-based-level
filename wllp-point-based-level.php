e3b4842c9837b9a7bad410a656e2e7d066abbd53
GokulakrishnanPalanisamy <gokulakrishnanpalanisamy12@gmail.com>
2025-03-18 17:00:35 +0530

    - add dependencies checker

--- wllp-point-based-level.php
+++ wllp-point-based-level.php
@@ -66,16 +66,13 @@ add_action('plugins_loaded', function () {
     );
     $myUpdateChecker->getVcsApi()->enableReleaseAssets();
 
-    add_action('wlr_before_init', function () {
-        if (defined('WLLP_PLUGIN_VERSION') && defined('WLLP_MINIMUM_WLR_VERSION')
-            && version_compare(WLLP_PLUGIN_VERSION, WLLP_MINIMUM_WLR_VERSION, '<=')
-        ) {
-            if (method_exists('\WLLP\App\Route', 'init')) {
-                do_action('wllp_before_init');
-                \WLLP\App\Route::init();
-                do_action('wllp_after_init');
-            }
-        }
-    });
+    if (method_exists('\WLLP\App\Helpers\Plugin', 'checkDependencies')
+        && \WLLP\App\Helpers\Plugin::checkDependencies()
+        && method_exists('\WLLP\App\Route', 'init')
+    ) {
+        do_action('wllp_before_init');
+        \WLLP\App\Route::init();
+        do_action('wllp_after_init');
+    }
 }, 5);
 
